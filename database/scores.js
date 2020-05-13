9d340a2e9577f1282b3d8777e5438615f9d27dd9
Braden <fish0859@gmail.com>
2020-05-13 10:44:00 -0600

    deleted db3

--- database/scores.js
+++ database/scores.js
@@ -2,7 +2,7 @@ const db = require("./model");
 const axios = require("axios");
 
 const updateScores = async () => {
-  let comments = await db.getComments(200, 0);
+  let comments = await db.getComments(10000, 0);
   console.log(comments);
   let scores = [];
   for (let i = 0; i < comments.length; i++) {
